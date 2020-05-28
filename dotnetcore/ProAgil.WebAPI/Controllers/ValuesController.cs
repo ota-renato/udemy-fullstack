7846e76990f9e76f03e1f6df86259610a57a8a5f
ota-renato <30956931+ota-renato@users.noreply.github.com>
2020-05-28 08:32:36 -0300

    Commit inicial do projeto

---
+++ dotnetcore/ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProAgil.WebAPI.Model;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Evento>> Get()
+        {
+            return new Evento[] { 
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular e .Net Core",
+                    Local = "São Paulo",
+                    Lote = "1º lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular9",
+                    Local = "São Paulo",
+                    Lote = "2º lote",
+                    QtdPessoas = 550,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                }
+             };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Evento> Get(int id)
+        {
+            return new Evento[] { 
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular e .Net Core",
+                    Local = "São Paulo",
+                    Lote = "1º lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular9",
+                    Local = "São Paulo",
+                    Lote = "2º lote",
+                    QtdPessoas = 550,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                }
+            }.FirstOrDefault(x => x.EventoId == id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
