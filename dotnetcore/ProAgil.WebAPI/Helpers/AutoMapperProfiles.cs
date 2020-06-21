b08e688e478957723595005a06ec15f859a8c177
ota-renato <30956931+ota-renato@users.noreply.github.com>
2020-06-21 00:41:02 -0300

    Inserindo Identity Core

--- dotnetcore/ProAgil.WebAPI/Helpers/AutoMapperProfiles.cs
+++ dotnetcore/ProAgil.WebAPI/Helpers/AutoMapperProfiles.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using AutoMapper;
 using ProAgil.Domain;
+using ProAgil.Domain.Identity;
 using ProAgil.WebAPI.Dtos;
 
 namespace ProAgil.WebAPI.Helpers
@@ -19,6 +20,9 @@ namespace ProAgil.WebAPI.Helpers
                 }).ReverseMap();
             CreateMap<Lote, LoteDto>().ReverseMap();
             CreateMap<RedeSocial, RedeSocialDto>().ReverseMap();
+
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, UserLoginDto>().ReverseMap();
         }
     }
 }
